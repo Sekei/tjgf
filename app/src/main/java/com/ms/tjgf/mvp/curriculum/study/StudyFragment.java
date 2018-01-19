3b358d35bb525e568dfcf105af439834ea0508d0
sekei <734473685@qq.com>
2018-01-19 18:38:56 +0800

    1.下拉列表效果

--- app/src/main/java/com/ms/tjgf/mvp/curriculum/study/StudyFragment.java
+++ app/src/main/java/com/ms/tjgf/mvp/curriculum/study/StudyFragment.java
@@ -1,25 +1,21 @@
 package com.ms.tjgf.mvp.curriculum.study;
 
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import com.ms.tjgf.R;
 import com.ms.tjgf.base.BaseFragment;
-import com.ms.tjgf.mvp.curriculum.study.adapter.IndicatorAdapter;
-import com.ms.tjgf.widget.CustomListView;
 import com.ms.tjgf.widget.SpinerPopWindow;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.PopupWindow.OnDismissListener;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * 太极自修课
@@ -29,7 +25,9 @@ import android.widget.Toast;
 public class StudyFragment extends BaseFragment implements OnDismissListener, OnItemClickListener, View.OnClickListener {
     private SpinerPopWindow<String> mSpinerPopWindow;
     private List<String> list;
-    private TextView mStudyName;
+    private Button mStudyName, mStudyCoach;
+    private Button mStudyTricks;
+    private int index = 0;//记录当前选中的
 
     @Override
     protected int getLayoutId() {
@@ -39,8 +37,13 @@ public class StudyFragment extends BaseFragment implements OnDismissListener, On
     @Override
     protected void initView(View view) {
         super.initView(view);
-        mStudyName = view.findViewById(R.id.study_name);
+        mStudyName = view.findViewById(R.id.study_series);
         mStudyName.setOnClickListener(this);
+        mStudyTricks = view.findViewById(R.id.study_tricks);
+        mStudyTricks.setOnClickListener(this);
+        mStudyCoach = view.findViewById(R.id.study_coach);
+        mStudyCoach.setOnClickListener(this);
+
         initData();
     }
 
@@ -48,7 +51,7 @@ public class StudyFragment extends BaseFragment implements OnDismissListener, On
      * 初始化数据
      */
     private void initData() {
-        list = new ArrayList<String>();
+        list = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             list.add("test:" + i);
         }
@@ -59,10 +62,26 @@ public class StudyFragment extends BaseFragment implements OnDismissListener, On
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.study_name:
+            case R.id.study_series:
+                index = 0;
+                mStudyName.setBackgroundResource(R.drawable.btn_rect_22_top_434351);
                 mSpinerPopWindow.setWidth(mStudyName.getWidth());
                 mSpinerPopWindow.showAsDropDown(mStudyName);
-                //setTextImage(R.drawable.icon_up);
+                setTextImage(R.drawable.icon_up);
+                break;
+            case R.id.study_tricks:
+                index = 1;
+                mStudyTricks.setBackgroundResource(R.drawable.btn_rect_22_top_434351);
+                mSpinerPopWindow.setWidth(mStudyTricks.getWidth());
+                mSpinerPopWindow.showAsDropDown(mStudyTricks);
+                setTextImage(R.drawable.icon_up);
+                break;
+            case R.id.study_coach:
+                index = 2;
+                mStudyCoach.setBackgroundResource(R.drawable.btn_rect_22_top_434351);
+                mSpinerPopWindow.setWidth(mStudyCoach.getWidth());
+                mSpinerPopWindow.showAsDropDown(mStudyCoach);
+                setTextImage(R.drawable.icon_up);
                 break;
         }
     }
@@ -72,12 +91,42 @@ public class StudyFragment extends BaseFragment implements OnDismissListener, On
      */
     @Override
     public void onDismiss() {
-        //setTextImage(R.drawable.icon_down);
+        if (index == 0) {
+            mStudyName.setBackgroundResource(R.drawable.btn_rect_43_434351);
+        } else if (index == 1) {
+            mStudyTricks.setBackgroundResource(R.drawable.btn_rect_43_434351);
+        } else {
+            mStudyCoach.setBackgroundResource(R.drawable.btn_rect_43_434351);
+        }
+        setTextImage(R.drawable.icon_down);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         mSpinerPopWindow.dismiss();
-        // tvValue.setText(list.get(position));
+        if (index == 0) {
+            mStudyName.setText(list.get(position));
+        } else if (index == 1) {
+            mStudyTricks.setText(list.get(position));
+        } else {
+            mStudyCoach.setText(list.get(position));
+        }
+    }
+
+    /**
+     * 给TextView右边设置图片
+     *
+     * @param resId
+     */
+    private void setTextImage(int resId) {
+        Drawable drawable = getResources().getDrawable(resId);
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());// 必须设置图片大小，否则不显示
+        if (index == 0) {
+            mStudyName.setCompoundDrawables(null, null, drawable, null);
+        } else if (index == 1) {
+            mStudyTricks.setCompoundDrawables(null, null, drawable, null);
+        } else {
+            mStudyCoach.setCompoundDrawables(null, null, drawable, null);
+        }
     }
 }
